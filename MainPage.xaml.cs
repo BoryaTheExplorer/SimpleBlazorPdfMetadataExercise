518ccc498d67f983457ad280434aeb773b305f15
Borys <boris.feldsher@live.com>
2025-04-07 13:15:12 +0200

    Initial commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorPdfMetadataExercise
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
